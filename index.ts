7108b40f81fcb60a7e84083d34b2a30f3f2c4123
Maria Eduarda <dudah.s3@gmail.com>
2023-10-08 01:33:55 -0300

    feat: adiciona menu iterativo

--- index.ts
+++ index.ts
@@ -1,12 +1,31 @@
-import { readCSV } from './model/readCSV';
-import { writeCSV } from './model/writeCSV';
-import { adicionarItem, listarItens, mediaPeso, mediaValor, pesoTotal, quantidadeItens, quantidadeProdutos, removerItem, valorTotal } from './controller/controleEstoque';
-import { rl, question } from './controller/controleEstoque';
-const menu = async () => {
+
+import {
+    adicionarProduto, removerProduto, listarItens, calcularValorTotal,
+    calcularPesoTotal, calcularQuantidadeItens, calcularQuantidadeProdutos,
+    calcularMediaValor, calcularMediaPeso
+} from './controller/controleEstoque';
+import { EstoqueItem } from './model/data.interface';
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const question = (query: string): Promise<string> => {
+    return new Promise((resolve) => {
+        rl.question(query, (answer) => {
+            resolve(answer);
+        });
+    });
+};
+
+
+function menu() {
     console.log("\n****** Menu de opções ******\n");
     console.log("0 - Sair");
-    console.log("1 - Adicionar um item");
-    console.log("2 - Remover um item");
+    console.log("1 - Adicionar um produto");
+    console.log("2 - Remover um produto");
     console.log("3 - Listar itens");
     console.log("4 - Calcular valor total");
     console.log("5 - Calcular peso total");
@@ -16,7 +35,7 @@ const menu = async () => {
     console.log("9 - Calcular média de peso");
 }
 
-const main = async () => {
+async function main() {
     let continuar = true;
 
     while (continuar) {
@@ -26,43 +45,62 @@ const main = async () => {
         switch (opcaoMenu) {
             case 0:
                 console.log("Saindo do programa.");
+                rl.close();
                 continuar = false;
                 break;
             case 1:
-                await adicionarItem();
+                const nome = await question('Nome do produto: ');
+                const peso = parseFloat(await question('Peso do produto (em kg): '));
+                const valor = parseFloat(await question('Valor do produto (em R$): '));
+                const quantidade = parseInt(await question('Quantidade disponível: '));
+                var novoProduto: EstoqueItem = { nome, peso, valor, quantidade, ativo: true };
+                await adicionarProduto(novoProduto);
                 break;
+
             case 2:
-                await removerItem();
+
+                console.log('Qual o nome do item que deseja remover?\n');
+                const id = await question('Nome do produto: ');
+                await removerProduto(id);
                 break;
+
             case 3:
                 await listarItens();
                 break;
+
             case 4:
-                await valorTotal();
+                await calcularValorTotal();
                 break;
+
             case 5:
-                await pesoTotal();
+                await calcularPesoTotal();
                 break;
+
             case 6:
-                await quantidadeItens();
+                await calcularQuantidadeItens();
                 break;
+
             case 7:
-                await quantidadeProdutos();
+                await calcularQuantidadeProdutos();
                 break;
+
             case 8:
-                await mediaValor();
+                await calcularMediaValor();
                 break;
+
             case 9:
-                await mediaPeso();
+                await calcularMediaPeso();
                 break;
+
             default:
-                console.log("Opção inválida");
+                console.log("Opção inválida!");
                 break;
         }
     }
 
     console.log("Obrigado por utilizar nosso sistema de gestão de estoque. Até mais!");
-    rl.close();
 };
 
 main();
+
+export { rl, question }
